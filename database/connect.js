bfe49a978c6867ef18da72d61655d415e2ab0157
Shivanshm001 <Shivanshmishra084@gmail.com>
2023-05-13 16:19:18 +0530

    Changed db connection from local to hosted

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: process.env.DB_RENDER_EXTERNAL_HOSTNAME,
+    host: process.env.DB_RENDER_HOSTNAME,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USERNAME,
